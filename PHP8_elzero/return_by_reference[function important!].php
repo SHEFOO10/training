f6cd8d4a458335d03b45423902256a1e941d7af6
unknown <445585913@alex4.moe.edu.eg>
2022-09-01 01:06:31 +0200

    simple update

--- PHP8_elzero/return_by_reference[function important!].php
+++ PHP8_elzero/return_by_reference[function important!].php
@@ -1,38 +1,41 @@
 <?php
-function &collector() {
-    $collection = array("shefoo"); // $collection = array(); this specificed collection variable as array 
-  // static to make collection hold data
-  return $collection;
-}
-$collection = &collector();
-$collection[] = 'foo'; // this will modified return of collector function and add to it 'foo'
-
-// array_push(collector(), "shefoo");  // this will modified return of collector function and add to it 'shefoo'
-// array_push need me to put keyword static on collection
-print_r($collection); // array( [0] => 'shefoo', [1] => 'foo')
-
-
-
-
-/*              returning by reference                                                   
-
-if i removed & from function this will raise error say only variable can be passed by reference
-
-
-    we need this syntax 
-        
-        function &name() {
-                $var;
-                return $var
+        function &collector() {
+                
+            $collection = array("shefoo"); // $collection = array(); this specificed collection variable as array 
+             // static to make collection hold data
+            return $collection;
         }
 
-        $ref = &name();
-        $ref = "string";
-        echo name(); // Result: string
-
-
         
-*/
+        $collection = &collector();
+        $collection[] = 'foo'; // this will modified return of collector function and add to it 'foo'
+
+        // array_push(collector(), "shefoo");  // this will modified return of collector function and add to it 'shefoo'
+        // array_push need me to put keyword static on collection
+        print_r($collection); // array( [0] => 'shefoo', [1] => 'foo')
+
+
+
+
+        /*              returning by reference                                                   
+
+        if i removed & from function this will raise error say only variable can be passed by reference
+
+
+            we need this syntax 
+                
+                function &name() {
+                        $var;
+                        return $var
+                }
+
+                $ref = &name();
+                $ref = "string";
+                echo name(); // Result: string
+
+
+                
+        */
 
 
 ?>
@@ -52,6 +55,4 @@ if i removed & from function this will raise error say only variable can be pass
         echo $myValue;                // prints the new value of $obj->value, i.e. 2.
 ?>
 
-
-?>
 
